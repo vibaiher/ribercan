947457c8e88171af9cca1ade27198af9b5b5ed11
Vicente Baixauli <vibaiher@gmail.com>
2013-09-18 19:26:50 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ribercan::Application.config.secret_key_base = '1c4baa46d1dc0608c454e3f7e6b3d10cd4a625b89090950b1fd5d7aa27467aad947b761f95cd5585b2ae61fc9536b846660d166bc9f2ef92db9cf53063cc5570'
